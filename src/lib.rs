087ce4e5f58bbd1632fb73725db5bf4275682417
Cerek Hillen <cerekh@gmail.com>
2023-04-29 23:09:14 -0700

    parse wheel names

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,56 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::str::FromStr;
+
+use lazy_static::lazy_static;
+use regex::Regex;
+
+pub struct WheelInfo {
+    pub distribution: String,
+    pub version: String,
+    pub build_tag: Option<String>,
+    pub python_tag: String,
+    pub platform_tag: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug)]
+pub enum WheelInfoParseError {
+    FailedMatch,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl FromStr for WheelInfo {
+    type Err = WheelInfoParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        lazy_static! {
+            static ref WHEEL_INFO_RE: Regex = Regex::new(concat!(
+                "^",
+                "(?P<distribution>.+)",
+                "-(?P<version>.+)",
+                "(-(?P<build_tag>.+))?",
+                "-(?P<python_tag>.+)",
+                "-(?P<platform_tag>.+)",
+                "\\.whl",
+                "$",
+            ))
+            .expect("Failed to parse wheel name regex");
+        }
+
+        let Some(captures) = WHEEL_INFO_RE.captures(s) else {
+	    return Err(WheelInfoParseError::FailedMatch);
+	};
+
+        Ok(Self {
+            distribution: must_named_capture(&captures, "distribution"),
+            version: must_named_capture(&captures, "version"),
+            build_tag: captures
+                .name("build_tag")
+                .map(|m| m.as_str())
+                .map(str::to_owned),
+            python_tag: must_named_capture(&captures, "python_tag"),
+            platform_tag: must_named_capture(&captures, "platform_tag"),
+        })
     }
+}
+
+fn must_named_capture(captures: &regex::Captures, name: &str) -> String {
+    captures.name(name).unwrap().as_str().to_owned()
 }
